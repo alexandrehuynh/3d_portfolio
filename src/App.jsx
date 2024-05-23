5b6c02d578db3c3f5964fcf5b871431811873c05
Alex Huynh <alexhuynh@Alexs-MacBook-Pro.local>
2024-05-23 15:28:10 -0700

    3d portfolio is rendered

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,32 @@
-import React from 'react'
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
-import NavBar from './components/NavBar'
-import { Home, About, Projects, Contact} from './pages'
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+
+import { Footer, NavBar } from "./components";
+import { About, Contact, Home, Projects } from "./pages";
+
 const App = () => {
   return (
     <main className='bg-slate-300/20'>
-        <Router>
-            <NavBar />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/projects" element={<Projects />} />
-                <Route path="/contact" element={<Contact />} />
-            </Routes>
-        </Router>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route
+            path='/*'
+            element={
+              <>
+                <Routes>
+                  <Route path='/about' element={<About />} />
+                  <Route path='/projects' element={<Projects />} />
+                  <Route path='/contact' element={<Contact />} />
+                </Routes>
+                <Footer />
+              </>
+            }
+          />
+        </Routes>
+      </Router>
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
