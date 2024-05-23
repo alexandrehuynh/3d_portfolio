ef9eac1ee82bcadca2a01be9ac10857c79e440a2
Alex Huynh <alexhuynh@Alexs-MacBook-Pro.local>
2024-05-23 15:18:49 -0700

    added components code

---
+++ src/components/index.js
@@ -0,0 +1,15 @@
+import CTA from "./CTA";
+import Alert from "./Alert";
+import Loader from "./Loader";
+import NavBar from "./NavBar";
+import Footer from "./Footer";
+import HomeInfo from "./HomeInfo";
+
+export {
+    CTA,
+    Alert,
+    Loader,
+    NavBar,
+    Footer,
+    HomeInfo
+}
